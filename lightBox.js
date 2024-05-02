fad27daf5a32a05a7a207c6499b398b412f3d2b4
Firuza Rahimova <firuza.rva@gmail.com>
2024-05-02 23:35:08 +0300

    2 plugins and 1 widget implemented

---
+++ lightBox.js
@@ -0,0 +1,8 @@
+<script>
+    $(document).ready(function(){
+        // Initialize all lightbox elements
+        $('[data-lightbox="light-box"]').lightbox()
+    })
+
+    
+</script>
---
+++ tooltip.js
@@ -0,0 +1,5 @@
+<script>
+    $(document).ready(function(){
+        $(document).tooltip()
+    })
+</script>
